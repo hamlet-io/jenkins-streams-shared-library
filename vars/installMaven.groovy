42dfe4cfa8f2d2dc68373a1e5c32fb14ab8a176c
Michael Leditschke <mike@ammd.com.au>
2021-03-12 13:07:45 +1100

    fix: maven step syntax

--- vars/installMaven.groovy
+++ vars/installMaven.groovy
@@ -1,6 +1,6 @@
 // Set the maven environment to the version specified
 def call(
-    String version = '3.6.3'
+    String version = '3.6.3',
     String checksum = 'c35a1803a6e70a126e80b2b3ae33eed961f83ed74d18fcd16909b2d44d7dada3203f1ffe726c17ef8dcca2dcaa9fca676987befeadc9b9f759967a8cb77181c0'
 ) {
     script {
