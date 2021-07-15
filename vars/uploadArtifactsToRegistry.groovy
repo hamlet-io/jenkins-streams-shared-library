a84b084300e239132a1c9af4d97c70e1f8b3ab79
roleyfoley <roleyfoley@gmail.com>
2021-07-15 04:42:01 +0000

    feat: image paths for upload
    Allows for provided an explicit image path that will be used as the
    source image when running manage images. This includes providing a
    directory that will be zipped for you

--- vars/uploadArtifactsToRegistry.groovy
+++ vars/uploadArtifactsToRegistry.groovy
@@ -3,7 +3,8 @@ def call(
     String deployment_unit,
     String scope,
     String image_formats,
-    String git_commit
+    String git_commit,
+    String image_paths = "",
 ) {
     // Inject parameters into the environment for the script steps
     // Variable names are treated as case insensitive so rename to avoid overwriting existing
@@ -13,6 +14,7 @@ def call(
         env['upload_scope'] = scope
         env['upload_image_formats'] = image_formats
         env['upload_git_commit'] = git_commit
+        env['upload_image_paths'] = image_paths
     }
 
     sh '''#!/bin/bash
@@ -25,8 +27,14 @@ def call(
     }
 
     sh '''#!/bin/bash
-        for upload_image_format in ${upload_image_formats}; do
-            ${AUTOMATION_DIR}/manageImages.sh -u "${upload_deployment_unit}" -c "${upload_scope}" -f "${upload_image_formats,,}" -g "${upload_git_commit}" || exit $?
+        for i in "${!upload_image_formats[@]}"; do
+            image_args=()
+
+            if [[ -n "${upload_image_paths[i]}" ]]; then
+                image_args+=("-i" "${upload_image_paths[i]}")
+            fi
+
+            ${AUTOMATION_DIR}/manageImages.sh -u "${upload_deployment_unit}" -c "${upload_scope}" -f "${upload_image_formats[i],,}" -g "${upload_git_commit}" "${image_args[@]} || exit $?
         done
     '''
 }
