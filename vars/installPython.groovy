c6b236adfa52c5dde1f4e806c66b8a91aac4c35d
Michael Leditschke <mike@ammd.com.au>
2021-10-23 15:05:25 +1100

    feat: nodejs/python scoping control
    When installing new versions of nodejs or python, permit the scope of
    the command to be controlled.

--- vars/installPython.groovy
+++ vars/installPython.groovy
@@ -1,9 +1,11 @@
 // Set the python environment to the version specified
 def call(
-    String version
+    String version,
+    String scope = "global"
 ) {
     script {
         env['required_python_version'] = version
+        env['required_scope'] = scope
     }
 
     // The agent may already have the required version installed
@@ -12,8 +14,11 @@ def call(
         if [[ ! ("${current_python_version}" =~ ${required_python_version}) ]]; then
             pyenv install "${required_python_version}" ||
                 { pyenv install --list; exit 1; }
-            pyenv global  "${required_python_version}" || exit $?
-            pip install --upgrade awscli pip virtualenv || exit $?
+            case ${required_scope} in
+              global|local)
+                pyenv ${required_scope} "${required_python_version}" || exit $?
+                ;;
+            esac
         fi
         pyenv version || exit $?
         pip install --upgrade awscli pip virtualenv || exit $?
