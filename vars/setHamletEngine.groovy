70c486991a89f9b703516e9d4edd85fc5b38d477
Michael Leditschke <mike@ammd.com.au>
2022-03-10 18:35:55 +1100

    fix: align to current hamlet cli behaviour
    Simplify the implementation to always force an
    install of the required hamlet engine version.

--- vars/setHamletEngine.groovy
+++ vars/setHamletEngine.groovy
@@ -13,20 +13,13 @@ def call(
     // The agent may already have the required version installed
     sh '''#!/bin/bash
 
-        pip install --upgrade "hamlet${hamlet_cli_version}"
-
-        current_hamlet_engine="$(hamlet engine get-engine)"
-        if [[ ! ("${current_hamlet_engine}" == ${required_hamlet_engine}) ]]; then
-            hamlet engine install-engine "${required_hamlet_engine}"
-        fi
-
-        if [[ "${update_hamlet_engine}" == "true" ]]; then
-            hamlet engine install-engine --update "${required_hamlet_engine}"
-        fi
-
-        hamlet engine set-engine "${required_hamlet_engine}"
-
+        echo "Updating the hamlet cli ..."
+        pip install --silent --upgrade "hamlet${hamlet_cli_version}"
         echo "hamlet version = \"$(hamlet --version)\""
+
+        echo "Updating the hamlet engine to ${required_hamlet_engine} ..."
+        hamlet engine install-engine --update "${required_hamlet_engine}"
+        hamlet engine set-engine "${required_hamlet_engine}"
         echo "hamlet engine = \"$(hamlet engine get-engine)\""
     '''
 }
