9b8b1313a1a9085f3e1984ef37dd14e3e56b8da4
Michael Leditschke <mike@ammd.com.au>
2022-03-10 15:45:42 +1100

    fix: hamlet cli version
    Rather than relying on the hamlet cli being present, or
    at a recent version, explicitly install the cli and
    request an upgrade.
    
    Add an optional parameter to provide the ability to
    contrain the version per standard pip syntax.

--- vars/setHamletEngine.groovy
+++ vars/setHamletEngine.groovy
@@ -1,15 +1,20 @@
 // Set the hamlet engine to use for
 def call(
     String engine,
-    Boolean update = false
+    Boolean update = false,
+    String cliVersion = ''
 ) {
     script {
         env['required_hamlet_engine'] = engine
         env['update_hamlet_engine'] = update
+        env['hamlet_cli_version'] = cliVersion
     }
 
     // The agent may already have the required version installed
     sh '''#!/bin/bash
+
+        pip install --upgrade "hamlet${hamlet_cli_version}"
+
         current_hamlet_engine="$(hamlet engine get-engine)"
         if [[ ! ("${current_hamlet_engine}" == ${required_hamlet_engine}) ]]; then
             hamlet engine install-engine "${required_hamlet_engine}"
@@ -20,5 +25,8 @@ def call(
         fi
 
         hamlet engine set-engine "${required_hamlet_engine}"
+
+        echo "hamlet version = \"$(hamlet --version)\""
+        echo "hamlet engine = \"$(hamlet engine get-engine)\""
     '''
 }
