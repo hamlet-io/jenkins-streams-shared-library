3e1ecee7425902f28375cb120584e0392f388a20
Michael Leditschke <mike@ammd.com.au>
2022-03-26 12:24:49 +1100

    feat: CMDB construction
    In preparation for converting the library functions to use the
    hamlet CLI, refactor properties handling to distinguish it
    from CMDB management.
    
    The repo associated with any job is now treated as the source of
    pipeline properties files. If the same repo is needed to construct
    a CMDB, it is separately loaded.
    
    A number of the previously hardcoded locations are now provided as
    default values to function parameters.

--- vars/loadCMDB.groovy
+++ vars/loadCMDB.groovy
@@ -1,28 +1,53 @@
-// Load a product CMDB to get access to the stream pipelines
+// Load a product repo to get access to the stream pipelines
+// Optionally load the accounts repo to permit hamlet based
+// deployments.
 def call(
-    String product_cmdb_url = '',
-    String branch = 'master',
-    String credentialsId = 'github'
+    String product_cmdb_url,
+    String product_cmdb_branch = 'master',
+    String credentialsId = 'github',
+    String accounts_cmdb_url = '',
+    String accounts_cmdb_branch = 'master',
+    String product_path = 'product',
+    String accounts_path = 'accounts',
+    String cmdb_path = ".hamlet/cmdb",
+    String cmdb_root_env_name = 'ROOT_DIR'
 ) {
-    // Product Setup
-    dir('.hamlet/product') {
-        script {
-            if (product_cmdb_url == '') {
-                // CMDB is configured as part of the pipeline
-                // Skip the default commit so we can force the value
-                // of GIT_COMMIT in the environment when updating build
-                // references.
-                checkout scm
-            } else {
-                // Need to load CMDB - don't include it in changelog calculations
+    dir( "${cmdb_path}" ) {
+
+        // Always load product repo as a minimum
+        // It can contain accounts info as well
+        dir( "${product_path}" ) {
+            script {
+                // Don't include it in changelog calculations
                 git(
                     url: product_cmdb_url,
-                    branch: branch,
+                    branch: product_cmdb_branch,
                     credentialsId: credentialsId,
                     changelog: false,
                     poll: false
                 )
             }
+        }
+
+        // Accounts Setup
+        dir( "${accounts_path}" ) {
+            script {
+                if ( accounts_cmdb_url ) {
+                    // Need to load accounts repo - don't include it in changelog calculations
+                    git(
+                        url: accounts_cmdb_url,
+                        branch: accounts_cmdb_branch,
+                        credentialsId: credentialsId,
+                        changelog: false,
+                        poll: false
+                    )
+                }
+            }
+        }
+
+        // Remember where the CMDB has been constructed
+        script {
+            env["${cmdb_root_env_name}"] = sh(script: 'pwd', returnStdout: true).trim()
         }
     }
 }
