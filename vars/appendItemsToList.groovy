1678f0984af7c4b747626ea7811c395e9f9ebc28
Michael Leditschke <mike@ammd.com.au>
2022-06-18 09:48:35 +1000

    feat: appendItemsToList
    Add function to abstract the vagaries of groovy when managing
    comma separated lists held in environment variables.
    
    Add the ability for library funnctions to honour environment
    overrides in segment properties files. This is initially being
    done to permit a segment to use a different hamlet version to others,
    but could equally be done to override things like account assignments.

---
+++ vars/appendItemsToList.groovy
@@ -0,0 +1,24 @@
+// Manage a comma separated list
+//
+// Main rationale is to deal with the vagaries of
+// environment variable access in Jenkinsfiles
+//
+// It leverages grrovy's "truthiness" rules
+//
+def call(
+    String list,
+    String items
+) {
+    if (list && (list.trim().size() != 0)) {
+        if (items && (items.trim().size() != 0)) {
+            return [list.trim(), items.trim()].join(',')
+        } else {
+            return list.trim()
+        }
+    } else {
+        if (items) {
+            return items.trim()
+        }
+    }
+    return ''
+}
